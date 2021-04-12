1b2c9e13dcd22d8ccab8d099a435d05683fd038b
Cyanide3D <gamer1997235@yandex.ru>
2021-04-12 20:18:56 +0300

    Еще кусок рефактора

--- src/main/java/com/cyanide3d/lib/mylittleorm/proxy/NewMethodParserImpl.java
+++ src/main/java/com/cyanide3d/lib/mylittleorm/proxy/NewMethodParserImpl.java
@@ -1,23 +1,35 @@
 package com.cyanide3d.lib.mylittleorm.proxy;
 
+import com.cyanide3d.Main;
 import com.cyanide3d.exception.InvalidInterfaceMethodSignatureException;
-import com.cyanide3d.lib.mylittleorm.database.DatabaseConnectionLayer;
-import com.cyanide3d.lib.mylittleorm.handler.DatabaseProvider;
+import com.cyanide3d.lib.mylittleorm.database.DatabaseLayer;
 import com.cyanide3d.lib.mylittleorm.handler.DaoRequestInvocationHandler;
+import com.cyanide3d.lib.mylittleorm.handler.DatabaseProvider;
 import com.cyanide3d.lib.mylittleorm.proxy.configurators.FindAllMethodConfigurer;
 import com.cyanide3d.lib.mylittleorm.proxy.configurators.FindByFieldMethodConfigurer;
 import com.cyanide3d.lib.mylittleorm.proxy.configurators.MethodConfigurer;
 import com.cyanide3d.lib.mylittleorm.proxy.configurators.SaveMethodConfigurer;
-import com.cyanide3d.lib.mylittleorm.query.sqlitedialect.SQLiteDialect;
+import com.cyanide3d.lib.mylittleorm.query.SQLDialect;
+import lombok.SneakyThrows;
 
 import java.lang.reflect.Method;
 import java.util.List;
+import java.util.Properties;
 
 public class NewMethodParserImpl implements MethodParser {
     private final DatabaseProvider dao;
 
     public NewMethodParserImpl() {
-        dao = new DaoRequestInvocationHandler(new SQLiteDialect(), new DatabaseConnectionLayer());
+        dao = getDao();
+    }
+
+    @SneakyThrows
+    private DatabaseProvider getDao() {
+        Properties properties = new Properties();
+        properties.load(Main.class.getResourceAsStream("/database.properties"));
+        SQLDialect dialect = (SQLDialect) Class.forName(properties.getProperty("database_dialect")).getDeclaredConstructor().newInstance();
+        DatabaseLayer dao = (DatabaseLayer) Class.forName(properties.getProperty("database_layer")).getDeclaredConstructor().newInstance();
+        return new DaoRequestInvocationHandler(dialect, dao);
     }
 
     @Override
